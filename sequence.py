a02f28d6fae6fd4e7892cfa6774493b67f005ce7
totason <jonkrona@gmail.com>
2018-09-04 11:03:13 +0000

    Lokid held eg

--- sequence.py
+++ sequence.py
@@ -1,18 +1,18 @@
 n = int(input("Enter the length of the sequence: "))
-x1 = 1
-x2 = 2
-x3 = 0
+x1 = 0
+x2 = 1
+x3 = 2
 xtemp = 0
 if n ==1:
-    print(x1)
+    print(x2)
 elif n == 2:
-    print(x1, x2)
+    print(x2, x3)
 else:
-    print(x1, x2, end =',')
-while 0 < n:
-    xtemp = x3
-    x3 = x1 + x2 + x3
-    print(x3, end = ',')
-    x1 = x2
-    x2 = xtemp
-    n -= 1
+    print(x2, x3, end =' ')
+    while 0 < n:
+        xtemp = x3
+        x3 = x1 + x2 + x3
+        print(x3, end = ' ')
+        x1 = x2
+        x2 = xtemp
+        n -= 1
