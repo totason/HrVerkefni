03f00945811e21db78d7fb2df7abc82035a9abae
totason <jonkrona@gmail.com>
2018-09-04 10:57:10 +0000

    fastur1

--- sequence.py
+++ sequence.py
@@ -2,12 +2,17 @@ n = int(input("Enter the length of the sequence: "))
 x1 = 1
 x2 = 2
 x3 = 0
-if n = 1:
+xtemp = 0
+if n ==1:
     print(x1)
-elif: n = 2:
+elif n == 2:
     print(x1, x2)
 else:
-    print(x1, x2)
+    print(x1, x2, end =',')
 while 0 < n:
+    xtemp = x3
     x3 = x1 + x2 + x3
-    print(x1, x2, x3)
+    print(x3, end = ',')
+    x1 = x2
+    x2 = xtemp
+    n -= 1
