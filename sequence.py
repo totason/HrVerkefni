df210cfc5f7f0918c7751edc076874b6ad6ba3f0
totason <jonkrona@gmail.com>
2018-09-04 11:12:12 +0000

    FullklaradOgFinpussad

--- sequence.py
+++ sequence.py
@@ -1,8 +1,5 @@
 n = int(input("Enter the length of the sequence: "))
-x1 = 0
-x2 = 1
-x3 = 2
-xtemp = 0
+x1, x2, x3, xtemp = 0, 1, 2, 0
 if n ==1:
     print(x2)
 elif n == 2:
@@ -16,6 +13,5 @@ else:
         xtemp = x3
         x3 = x1 + x2 + x3
         print(x3)
-        x1 = x2
-        x2 = xtemp
+        x1, x2 = x2, xtemp
         n -= 1
