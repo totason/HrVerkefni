25b4383f68c939e4150801087c40dcfdf6d47d8c
totason <jonkrona@gmail.com>
2018-09-04 11:06:16 +0000

    Lokid i alvoru

--- sequence.py
+++ sequence.py
@@ -6,13 +6,16 @@ xtemp = 0
 if n ==1:
     print(x2)
 elif n == 2:
-    print(x2, x3)
+    print(x2)
+    print(x3)
 else:
-    print(x2, x3, end =' ')
+    print(x2)
+    print(x3)
+    n -=2
     while 0 < n:
         xtemp = x3
         x3 = x1 + x2 + x3
-        print(x3, end = ' ')
+        print(x3)
         x1 = x2
         x2 = xtemp
         n -= 1
