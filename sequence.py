13b18ce1a5824e9cb79f86c561fd05a39bbf3882
totason <jonkrona@gmail.com>
2018-09-04 10:50:49 +0000

    Byrjun

---
+++ sequence.py
@@ -0,0 +1,13 @@
+n = int(input("Enter the length of the sequence: "))
+x1 = 1
+x2 = 2
+x3 = 0
+if n = 1:
+    print(x1)
+elif: n = 2:
+    print(x1, x2)
+else:
+    print(x1, x2)
+while 0 < n:
+    x3 = x1 + x2 + x3
+    print(x1, x2, x3)
